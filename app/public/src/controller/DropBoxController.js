756d6b33227dbfc2a6642311f2ade0508c7efabd
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-03-28 19:59:35 -0300

    feat: delete file

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -40,7 +40,38 @@ class DropBoxController {
     return this.listFilesEl.querySelectorAll('.selected');
   }
 
+  removeTask() {
+    let promises = [];
+
+    this.getSelection().forEach((li) => {
+      let file = JSON.parse(li.dataset.file);
+      let key = li.dataset.key;
+
+      let formData = new FormData();
+
+      formData.append('path', file.path);
+      formData.append('key', key);
+
+      promises.push(this.ajax('/file', 'DELETE', formData));
+    });
+    return Promise.all(promises);
+  }
+
   initEvents() {
+    this.btnDelete.addEventListener('click', (e) => {
+      this.removeTask()
+        .then((responses) => {
+          responses.forEach((response) => {
+            if (response.fields.key) {
+              this.getFirebaseRef().child(response.fields.key).remove();
+            }
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
     this.btnRename.addEventListener('click', (e) => {
       let li = this.getSelection()[0];
       console.log(li);
@@ -96,6 +127,37 @@ class DropBoxController {
     });
   }
 
+  ajax(
+    url,
+    method = 'GET',
+    formData = new FormData(),
+    onprogress = function () {},
+    onloadstart = function () {},
+  ) {
+    return new Promise((resolve, reject) => {
+      let ajax = new XMLHttpRequest();
+
+      ajax.open(method, url);
+
+      ajax.onload = (event) => {
+        try {
+          resolve(JSON.parse(ajax.responseText));
+        } catch (e) {
+          reject(e);
+        }
+      };
+      ajax.onerror = (event) => {
+        reject(event);
+      };
+
+      ajax.upload.onprogress = onprogress;
+
+      onloadstart();
+
+      ajax.send(formData);
+    });
+  }
+
   uploadComplete() {
     this.modalShow(false);
     this.inputFilesEl.value = '';
@@ -115,35 +177,22 @@ class DropBoxController {
 
     //files é uma colecao tem que converter em array
     [...files].forEach((file) => {
+      let formData = new FormData();
+
+      formData.append('input-file', file);
+
       promises.push(
-        new Promise((resolve, reject) => {
-          let ajax = new XMLHttpRequest();
-
-          ajax.open('POST', '/upload');
-
-          ajax.onload = (event) => {
-            try {
-              resolve(JSON.parse(ajax.responseText));
-            } catch (e) {
-              reject(e);
-            }
-          };
-          ajax.onerror = (event) => {
-            reject(event);
-          };
-
-          ajax.upload.onprogress = (event) => {
+        this.ajax(
+          '/upload',
+          'POST',
+          formData,
+          () => {
             this.uploadProgress(event, file);
-          };
-
-          let formData = new FormData();
-
-          formData.append('input-file', file);
-
-          this.startUploadTime = Date.now();
-
-          ajax.send(formData);
-        }),
+          },
+          () => {
+            this.startUploadTime = Date.now();
+          },
+        ),
       );
     });
 
